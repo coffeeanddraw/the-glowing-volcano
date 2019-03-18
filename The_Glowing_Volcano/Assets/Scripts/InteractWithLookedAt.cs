dc3c81ddcb770c75ac2c8ccec5a2b933e814c6d8
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2019-03-18 02:39:39 -0500

    Add InteractWithLookedAt.cs

---
+++ The_Glowing_Volcano/Assets/Scripts/InteractWithLookedAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects when the player presses the interact button while looking at an IInteractive 
+/// and then calls that IInteractive's Interactive method
+/// </summary>
+
+public class InteractWithLookedAt : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update() // check every frame 
+    {
+        
+    }
+}
