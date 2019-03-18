788815ea0f3e0bb908e87c15257ae631e6753ce2
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2019-03-18 01:39:05 -0500

    Add InteractiveObject.cs

---
+++ The_Glowing_Volcano/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour, IInteractive
+{
+    public void InteractWith()
+    {
+        Debug.Log("Player just interacted with " + gameObject.name);
+    }
+}
