050b6cc876b87aabac659e6a372a7745a6c36e5a
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2019-03-20 20:53:45 -0500

    Add sound effects

--- The_Glowing_Volcano/Assets/Scripts/InteractiveObject.cs
+++ The_Glowing_Volcano/Assets/Scripts/InteractiveObject.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
     [SerializeField]
     private string displayText = nameof(InteractiveObject);
     public string DisplayText => displayText;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void InteractWith()
     {
+        try
+        {
+            audioSource.Play();
+        }
+        catch (System.Exception)
+        {
+            throw new System.Exception("Interactive Object is missing an audio source component");
+        }
+
         Debug.Log($"Player just interacted with {gameObject.name}.");
     }
 }
