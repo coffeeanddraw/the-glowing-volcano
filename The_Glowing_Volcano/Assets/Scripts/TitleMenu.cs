8e5fb32fea0799ceff58ee4847f3b79aff843c96
Three Eyed Venelope <threeeyedvenelope@gmail.com>
2019-04-04 02:21:16 -0500

    Add title menu functionality

---
+++ The_Glowing_Volcano/Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string gameSceneName;
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
